0caf220d5b90be476294dca23722f91a5fa20d77
이유정 <epoint8989@gmail.com>
2025-03-28 14:14:54 +0900

    시작코드 주석 작성

--- src/main/java/study/group/StudyApplication.java
+++ src/main/java/study/group/StudyApplication.java
@@ -5,9 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
+//@Scheduled 어노테이션을 사용할 수 있도록 설정
 @SpringBootApplication
+//@ComponentScan, @EnableAutoConfiguration, @Configuration을 포함, 자동으로 Bean 스캔하고 설정을 적용
+//Bean은 Spring IoC 컨테이너에서 자동으로 관리되는 객체
 public class StudyApplication {
   public static void main(String[] args) {
     SpringApplication.run(StudyApplication.class, args);
+    //Spring Boot 애플리케이션을 시작하는 코드
   }
 }
